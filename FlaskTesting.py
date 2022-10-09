a02fa0837e239812ab4bd9cfc76bfd2139afa8c5
enrval <enriquefvalentino@gmail.com>
2022-10-08 23:43:37 -0400

    static leaflet page is now being served by Flask

--- FlaskTesting.py
+++ FlaskTesting.py
@@ -1,11 +1,13 @@
 # follow this to configure the environment
 # https://www.tutorialspoint.com/flask/flask_environment.htm
-# running this file will open the development server at http://127.0.0.1:5000
+# running this file will open the development server at http://localhost:3000 in debug mode
 # test routes are:
-# http://127.0.0.1:5000
-# http://127.0.0.1:5000/test
+# http://localhost:3000
+# http://localhost:3000/test
+# http://localhost:3000/leaflet (this serves the static leaflet html page)
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,5 +18,9 @@ def home():
 def test():
    return "Test page"
 
+@app.route('/leaflet')
+def serve_leaflet():
+   return render_template('leaflet_testing.html')
+
 if __name__ == '__main__':
-   app.run()
+   app.run(host='localhost', port='3000', debug=True)
