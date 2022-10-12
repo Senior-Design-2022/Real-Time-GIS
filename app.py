ad81d8050d53f3c257bbae5a08c54b9e917aa70a
Ryan Clark <ryanclark.home@gmail.com>
2022-10-12 14:45:32 -0400

    added paths and reorganized file structure
    made separate scripts.js file, added paths, changed home route

--- app.py
+++ app.py
@@ -4,30 +4,19 @@
 # test routes are:
 # http://localhost:3000
 # http://localhost:3000/test
-# http://localhost:3000/leaflet (this serves the static leaflet html page)
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-   return "Home page"
+def serve_leaflet():
+   return render_template('index.html')
 
 @app.route('/test')
 def test():
    return "Test page"
 
-@app.route('/leaflet')
-def serve_leaflet():
-   markers = [
-      {
-         'lat':40.745152,
-         'lon':-74.024345,
-         'popup':'Quack'
-      }
-   ]
-   return render_template('index.html', markers=markers)
 
 if __name__ == '__main__':
    app.run(host='localhost', port='3000', debug=True)
---
+++ static/script.js
@@ -0,0 +1,44 @@
+var map = L.map('map').setView([40.745152, -74.024345], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+//
+// basic marker
+//
+
+markers = [
+    {
+        'lat':40.745152,
+        'lon':-74.024345,
+        'popup':'Quack'
+    }
+]
+
+for (marker of markers) {
+    console.log(marker)
+    L.marker([marker.lat, marker.lon]).addTo(map)
+    .bindPopup(marker.popup)
+    .openPopup();
+}
+
+//
+// creating a path
+//
+
+latlngs = [
+    [40.742029, -74.028380],
+    [40.745784, -74.027226],
+    [40.745602, -74.026211],
+    [40.747672, -74.025585],
+    [40.747335, -74.023922],
+    [40.745689, -74.023209],
+    [40.744230, -74.023574],
+    [40.742885, -74.025838],
+    [40.741759, -74.026562]
+]
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+map.fitBounds(polyline.getBounds());
+    
