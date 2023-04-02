c558395bb63cf72b4ffc0ebad7a881abf0b9dffa
Ryan Clark <ryanclark.home@gmail.com>
2023-04-01 21:21:02 -0400

    replays working from front end

--- app.py
+++ app.py
@@ -4,15 +4,17 @@
 # test routes are:
 # http://localhost:3000
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, jsonify
 from flask_sock import Sock
 from pathlib import Path
 import json
 import xml.etree.ElementTree as ET
+import os.path
 
 from CoTUtils.CoTUtility import CoTUtility
 from SocketUtils.SocketUtils import SITQueuedUDPClient
 from Logger.SaveReplay import SaveReplay
+from Logger.Replay import Replay
 
 # for colored prints
 class bcolors:
@@ -31,12 +33,48 @@ app = Flask(__name__, static_folder='static')
 sock = Sock(app)
 
 # initialize logger for saving replays
-logger = SaveReplay(debug=True)
+debugLogger = True
+recording = False
+logger = None
+
 
 # render home page
 @app.route('/')
 def serve_leaflet():
    return render_template('index.html')
+
+# start recording input feed
+@app.route('/recording/start')
+def start_recording():
+   recording = True
+   logger = SaveReplay(debug=debugLogger)
+   return "Started recording input feed"
+   
+
+# stop recording input feed
+@app.route('/recording/stop')
+def stop_recording():
+   # stop the recording and create a new logger -- i.e. a new log file
+   recording = False
+   return "Stopped recording input feed"
+
+# runs a replay based on a file path
+#
+# TODO: figure out how to send JSON responses back for better error handling on the front end
+#
+@app.route('/replay', methods=['POST'])
+def run_replay():
+   path = request.get_json()['path']
+   speed = request.get_json()['speed']
+      
+   print(path)
+   
+   print("starting replay")
+   replay = Replay(path, '127.0.0.1', 1870, float(speed))
+   print("replay started")   
+
+   return "success" 
+
 
 @sock.route('/feed')
 def feed(sock):
@@ -52,7 +90,9 @@ def feed(sock):
                   # raw input
                   print(f"\n{bcolors.HEADER}raw:{bcolors.ENDC}")
                   print(item)
-                  logger.write_to_log(item[1])
+
+                  if recording: 
+                     logger.write_to_log(item[1])
 
                   # parse to front end format
                   parsed = CoTUtility.parseData(item[1], wanted_params)
